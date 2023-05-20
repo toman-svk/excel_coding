c9127ed159ffa661ed796c7a93ce210d771221e2
Filip Tomanka <tomanka.filip@gmail.com>
2023-05-20 12:38:28 +0200

    Automatic commit, 2023-05-20 12:38:28.171718

--- git_commit.py
+++ git_commit.py
@@ -10,10 +10,13 @@ repo = git.Repo(project_folder)
 
 # Add all changes to the staging area
 repo.git.add('--all')
+print('Added everything to stage.')
 
 # Create a commit with the current date and time as the message
 commit_message = f'Automatic commit, {datetime.now()}'
 repo.git.commit('-m', commit_message)
+print('Commited everything.')
 
 # Push the changes to the repository
 repo.remotes.origin.push()
+print('Pushed everything.')
