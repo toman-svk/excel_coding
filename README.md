f3d98ce1e1def2d21276211d4735decc471301ae
Filip Tomanka <tomanka.filip@gmail.com>
2023-05-20 12:37:12 +0200

    Automatic commit, 2023-05-20 12:37:12.831594

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Notes for future development:
 - function to find out the primary key in the table
+-- loop through columns, and if values in column are unique, it may be a primary key column.  Columns with smaller index (at the beginning) have priority?
---
+++ git_commit.py
@@ -0,0 +1,19 @@
+import os
+import git
+from datetime import datetime
+
+# Specify the project folder path
+project_folder = 'B:\Programming\excel_coding'
+
+# Initialize a Git repository object
+repo = git.Repo(project_folder)
+
+# Add all changes to the staging area
+repo.git.add('--all')
+
+# Create a commit with the current date and time as the message
+commit_message = f'Automatic commit, {datetime.now()}'
+repo.git.commit('-m', commit_message)
+
+# Push the changes to the repository
+repo.remotes.origin.push()
